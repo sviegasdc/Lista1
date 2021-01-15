6c2a1b178abb487c4eb9a4d4d9c975b0e9ab35fd
sacrs137 <77491046+sacrs137@users.noreply.github.com>
2021-01-15 09:06:55 -0300

    Create Ex03.cs

---
+++ Ex03.cs
@@ -0,0 +1,12 @@
+using System;
+ 
+class MainClass {
+ public static void Main (string[] args) {
+  
+   Console.WriteLine ("Digite seu primeiro nome: ");
+ 
+   string nome = Console.ReadLine ();
+ 
+   Console.WriteLine ("Bem-vindo ao C#, " + nome);
+ }
+}
